be4b455a787f4dfe09aadb78ee9b10d74fb74e06
hyenON <hy3782@gmail.com>
2025-11-23 18:05:09 +0900

    feat: 운영기관 회원가입(org/register) 인증 예외처리 및 multipart 대응 로직 수정

--- src/main/java/org/example/socam_be/config/CorsConfig.java
+++ src/main/java/org/example/socam_be/config/CorsConfig.java
@@ -9,7 +9,12 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("localhost:3306") //프론트 주소
+                .allowedOrigins(
+                        "http://localhost:3000",  // 너
+                        "http://localhost:5173",  // 팀원(대표적으로 Vite)
+                        "http://localhost:8080",  // 혹시 다른 팀원
+                        "http://localhost:3306"   // 기존 팀원 코드 유지 (실제 의미는 없으나 '그대로 둠')
+                ) //프론트 주소
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
