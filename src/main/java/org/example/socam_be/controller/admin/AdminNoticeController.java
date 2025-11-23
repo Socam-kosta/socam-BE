db6e72fa8d1a527e5a648863eaf92709cb962ee5
hyenON <hy3782@gmail.com>
2025-11-23 21:00:39 +0900

    공지사항 조회 추가

--- src/main/java/org/example/socam_be/controller/admin/AdminNoticeController.java
+++ src/main/java/org/example/socam_be/controller/admin/AdminNoticeController.java
@@ -8,10 +8,9 @@ import org.example.socam_be.dto.admin.NoticeResponseDto;
 import org.example.socam_be.service.admin.AdminNoticeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin/notices")
@@ -21,6 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminNoticeController {
 
     private final AdminNoticeService adminNoticeService;
+
+    // 전체 공지사항 목록 조회
+    @GetMapping
+    public ResponseEntity<List<NoticeResponseDto>> getAllNotices() {
+        List<NoticeResponseDto> notices = adminNoticeService.getAllNotices();
+        return ResponseEntity.ok(notices);
+    }
 
     //✅ [ADM006] 공지사항 등록 API
     @PostMapping
